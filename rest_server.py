a07b813b68c4997321f13c5272177f0686e5559e
syntactix <ccrunner2013@gmail.com>
2017-12-07 18:58:19 -0500

    Gunicorn reader

--- rest_server.py
+++ rest_server.py
@@ -71,8 +71,5 @@ def add_malware():
         list_malware()
 
 if __name__ == '__main__':
-    if MACHINE == 'local':
-        app.run(debug=True, port=5000)
-    else:
-        app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, port=5000)
     
